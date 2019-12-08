bee3bd869bb8919f8c10ce0aa82b1c9d9c0385a9
linkfly <s.katrevich@lingualeo.com>
2019-12-08 20:33:28 +0300

    in-progress

--- main.cpp
+++ main.cpp
@@ -1,14 +1,93 @@
 ﻿#include "share.h"
 
 #include <iostream>
+#include <cassert>
 
 #include "matrix.h"
 
 using std::cout;
 using std::endl;
 
+template<typename T, int defval>
+std::ostream& operator<<(std::ostream& out, const MatrixIterator<T, defval>& iter) {
+	out << *iter;
+	return out;
+}
+
 int main() {
-	
-	
+
+	//Matrix<int, 0> mm;
+	//MatrixIterator<int, 0> myit = mm(0, 0);
+	//auto it = myit.find(34);
+	///**myit;*/
+	//cout << myit << endl;
+
+	//Matrix<int, 0> mtx;
+	//auto it = mtx(0, 0);
+	//cout << (*it);
+	//return 0;
+
+	/*Trivial checking code*/
+	{
+		Matrix<int, -1> matrix; // бесконечная матрица int заполнена значениями -1
+		assert(matrix.size() == 0); // все ячейки свободны
+		auto a = matrix(0, 0);
+		assert(a == -1);
+		assert(matrix.size() == 0);
+		matrix(100, 100) = 314;
+		assert(matrix(100, 100) == 314);
+		assert(matrix.size() == 1);
+	}
+	///////////////////
+
+	/* При запуске программы необходимо создать матрицу с пустым значением 0 */
+	Matrix<int, 0> m;
+
+	const int N = 10;
+	auto half_side = N / 2;
+	auto defval = decltype(m)::defval;
+
+	/* заполнить диагонали матрицы
+	выражением m[i][i] = m[N - 1 - i][i] = i; // N = 10, i = [0, N) */
+	for (int i = 0; i < N; i++) {
+		m[i][i] = m[N - 1 - i][i] = i;
+		m[i][N - 1 - i] = m[N - 1 - i][N - 1 - i] = i;
+	}
+
+	/* Начиная с ячейки [0, 0] в шахматном порядке заполнить матрицу 10x10 значением по умолчанию.
+	Заполнение в шахматном порядке заняло 25 итераций */
+	/* (ниже альтернативное, оптимизированное решение) */
+	//for (int i = 0; i < half_side; i++) {
+	//	auto cur_row = i * 2;
+	//	for (int j = 0; j < half_side; j++) {
+	//		auto cur_col = j * 2;
+	//		m[cur_row][cur_col] = defval;
+	//		m[cur_row + 1][cur_col + 1] = defval;
+	//	}
+	//}
+
+	/* !!! Альтернативное решение, так как мы по факту "стираем" только одну диагоняль (записываем значения по-умолчанию),
+	то мы вполне можем просто стереть эту диагоняль, т.е. решить задачу за 10 итераций!!! */
+	for (int i = 0; i < N; i++) {
+		m[i][N - 1 - i] = m[N - 1 - i][N - 1 - i] = defval;
+	}
+
+
+	/* Необходимо вывести фрагмент матрицы от [1,1] до [8,8]. Между столбцами пробел. Каждая строка матрицы
+	на новой строке. */
+	for (int i = 1; i <= 8; i++) {
+		cout << m[i][1];
+		for (int j = 2; j <= 8; j++) {
+			cout << " " << m[i][j];
+		}
+		cout << endl;
+	}
+
+	cout << m.size() << endl;
+
+	for (int i = 1; i <= 8; i++) {
+
+	}
+
 	return 0;
 }
--- matrix.h
+++ matrix.h
@@ -2,4 +2,118 @@
 
 #include "share.h"
 
+#include <map>
+#include <iostream>
 
+using std::map;
+
+template<typename T, int defval>
+class Matrix;
+
+template<typename T, int defval>
+struct MatrixIterator {
+	MatrixIterator(Matrix<T, defval>* parent, unsigned row_idx, unsigned col_idx) :
+		parent{ parent }, cur_row{ row_idx }, cur_col{ col_idx } {}
+	T operator*() const {
+		return parent->get(cur_row, cur_col);
+	}
+	bool operator==(const T& right) {
+		return parent->get(cur_row, cur_col) == right;
+	}
+
+	MatrixIterator& operator=(const T& val) {
+		parent->add(cur_row, cur_col, val);
+		return *this;
+	}
+
+	auto find(unsigned key) {
+		return parent->data.find(key);
+	}
+
+private:
+	Matrix<T, defval>* parent;
+	unsigned cur_row;
+	unsigned cur_col;
+};
+
+template<typename T, int defval>
+class Matrix {
+	using row = map<unsigned, T>;
+	using cur_iterator = MatrixIterator<T, defval>;
+	friend struct MatrixIterator<T, defval>;
+	map<unsigned, row> data;
+	size_t length = 0;
+
+	bool getRow(unsigned row_idx, row*& row) {
+		auto it = data.find(row_idx);
+		if (it != data.end()) {
+			row = &it->second;
+			return true;
+		}
+		return false;
+	}
+
+public:
+	constexpr static decltype(defval) defval = defval;
+	struct part_iterator {
+		part_iterator(Matrix<T, defval>* parent, unsigned row_idx) :
+			parent{ parent }, cur_row{ row_idx } {}
+		cur_iterator operator[](const unsigned& col_idx) {
+			return MatrixIterator<T, defval>{ parent, cur_row, col_idx };
+		}
+	private:
+		Matrix<T, defval>* parent;
+		unsigned cur_row;
+	};
+
+	T get(unsigned row_idx, unsigned col_idx) {
+		row* prow;
+		if (getRow(row_idx, prow)) {
+			auto it = prow->find(col_idx);
+			if (it != prow->end()) {
+				return it->second;
+			}
+		}
+		return defval;
+	}
+
+	void add(unsigned row_idx, unsigned col_idx, const T& elt) {
+		row* prow;
+		if (getRow(row_idx, prow)) {
+			auto it = prow->find(col_idx);
+			if (it != prow->end()) {
+				it->second = elt;
+				elt == defval && length--;
+			}
+			else {
+				if (elt == defval) return;
+				length++;
+				(*prow)[col_idx] = elt;
+			}
+		}
+		else {
+			if (elt == defval) return;
+			length++;
+			// data destructor will do to free memory
+			auto new_row = new row{};
+			(*new_row)[col_idx] = elt;
+			data.emplace(row_idx, *new_row);
+		}
+	}
+
+	size_t size() {
+		return length;
+	}
+
+	cur_iterator operator()(unsigned row_idx, unsigned col_idx) {
+		return cur_iterator{ this, row_idx, col_idx };
+	}
+	
+	part_iterator operator[](unsigned row_idx) {
+		return part_iterator{ this, row_idx };
+	}
+
+	/*MatrixIterator& begin() {
+		return MatrixIterator{ this, 0, 0 };
+	}*/
+};
